57e1f3e63dd6a11fd85b43774296d6309592288f
Lukasz Kielczykowski <kielczykowski.lukasz@gmail.com>
2015-07-24 19:52:17 +0200

    Simple movement for boss

---
+++ sgj/Assets/Scripts/Boss/moveable.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveable : MonoBehaviour {
+
+    public float speed = 0;
+
+	void Update () {
+        if (Input.GetKey(KeyCode.W)) {
+            transform.position += new Vector3(0, CalculateSpeed(), 0);
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+            transform.position += new Vector3(0, -CalculateSpeed(), 0);
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            transform.position += new Vector3(-CalculateSpeed(), 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            transform.position += new Vector3(CalculateSpeed(), 0, 0);
+        }
+	}
+
+    float CalculateSpeed() {
+        if (Time.deltaTime > 0) {
+            return speed / Time.deltaTime;
+        } else {
+            return 0;
+        }
+    }
+}
