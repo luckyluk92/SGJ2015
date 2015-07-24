1323b28ab9b8f4be4dd82e4275b7f6a561a0e025
Daniel <daniellewinski95@gmail.com>
2015-07-25 00:05:40 +0200

    Update: employee.cs, GameState.cs Added employees' wages.

--- sgj/Assets/Scripts/GameState.cs
+++ sgj/Assets/Scripts/GameState.cs
@@ -3,36 +3,51 @@ using System.Collections;
 
 public class GameState : MonoBehaviour {
 
-	public int dayTime = 60;
-	float currentTime;
+	public float dayTime = 60f;
+	private float _currentTime;
+
+	public float wage = 100;
+
+	//[HideInInspector]
+	public float numberOfEmployees;
 
 	public float money;
 	
 	// Use this for initialization
 	void Start () 
 	{
-		currentTime = 0;
+		_currentTime = 0;
 		money = 0;
+		numberOfEmployees = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		TimeFlow();
-
 		//Debug.Log(currentTime + " " + money);
 	}
 
 	void TimeFlow()
 	{
-		if( currentTime < dayTime)
+		if( _currentTime < dayTime)
 		{
-			currentTime += Time.deltaTime;
+			_currentTime += Time.deltaTime;
 		}
 		else
 		{
-			currentTime = 0;
+			_currentTime = 0;
+			money -= numberOfEmployees * wage;
+			isThisTheEnd();
 			//Load next Day
 		}
-	}	
+		Debug.Log(money);
+	
+	}
+
+	void isThisTheEnd()
+	{
+		if(money <= 0)
+			Debug.Log("Game Over"); //TODO
+	}
 }
