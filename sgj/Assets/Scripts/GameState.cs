8c290b8a54964b8dba95a30061d55620d32370f2
Daniel <daniellewinski95@gmail.com>
2015-07-24 22:16:57 +0200

    Employee.cs: Added money counting.
    GameState.cs:
    Added day cycles.

---
+++ sgj/Assets/Scripts/GameState.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+
+	public int dayTime = 60;
+	float currentTime;
+
+	public int money;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		currentTime = 0;
+		money = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		TimeFlow();
+
+		//Debug.Log(currentTime + " " + money);
+	}
+
+	void TimeFlow()
+	{
+		if( currentTime < dayTime)
+		{
+			currentTime += Time.deltaTime;
+		}
+		else
+		{
+			currentTime = 0;
+			//Load next Day
+		}
+	}	
+}
