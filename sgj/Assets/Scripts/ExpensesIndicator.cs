ec51a930e680589624424ead7bc1985dc8ae4611
Nitwoe <szymi142@gmail.com>
2015-07-25 09:27:06 +0200

    Expenses indicator

---
+++ sgj/Assets/Scripts/ExpensesIndicator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ExpensesIndicator : MonoBehaviour {
+
+    GameState state;
+    Text expensesText;
+    
+	// Use this for initialization
+	void Start () {
+        state = Camera.main.GetComponent<GameState>();
+        expensesText = gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        expensesText.text = string.Format("Expenses: {0:C}", Convert.ToDecimal(state.Expenses));
+	}
+}
