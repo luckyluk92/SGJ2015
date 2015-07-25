e5337c2605bede8ce6c810a52b2fb657da66fb32
Lukasz Kielczykowski <kielczykowski.lukasz@gmail.com>
2015-07-25 04:48:54 +0200

    Range with animation and shit :pig:

---
+++ sgj/Assets/Scripts/SpriteFader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class SpriteFader : MonoBehaviour {
+
+    public UnityEvent onDestroy;
+
+    public void Kill() {
+        Destroy(gameObject);
+        onDestroy.Invoke();
+    }
+}
