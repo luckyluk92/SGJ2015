3c020abab0c466610e067b1fb527e92056380e93
Daniel <daniellewinski95@gmail.com>
2015-07-25 13:22:47 +0200

    Added phone functionality

---
+++ sgj/Assets/Scripts/Phone.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Phone : MonoBehaviour {
+
+	public bool isRinging = false;
+	public bool isBossSpeaking;
+
+	public GameObject progressBar;
+
+	public int minSpeakingTime = 2;
+	public int maxSpeakingTime = 7;
+	private int speakingTime;
+
+	public float moneyPerSecond = 50;
+
+	private float _currentTime;
+	private float _totalTime;
+	public float ringingTime;
+
+	public float ringingChanceThreshold = 8.9f;
+	
+
+	private GameState _gameState;
+	// Use this for initialization
+	void Start () {
+		isBossSpeaking = false;
+		_gameState = Camera.main.GetComponent<GameState>();
+		ringingTime = 5;
+		_currentTime = 0;
+
+		progressBar.GetComponent<StressProgressBar>().isVisible = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if((int)_gameState.currentTime % (int)(_gameState.dayTime/4) == 0)
+		{
+			float diceThrow = Random.Range(0,10);
+			if(diceThrow > ringingChanceThreshold && !isBossSpeaking)
+				isRinging = true;
+		}
+
+
+		if(isBossSpeaking)
+		{
+			_currentTime = Time.deltaTime;
+			_totalTime += _currentTime;
+		}
+		else if(isRinging)
+		{
+			_currentTime += Time.deltaTime;
+
+			if(_currentTime >= ringingTime)
+			{
+				isRinging = false;
+				_currentTime = 0;
+			}
+		}
+
+
+
+		progressBar.SendMessage("ProgressUpdated", _totalTime/speakingTime);
+		_totalTime = Mathf.Clamp(_totalTime, 0, speakingTime);
+	}
+
+	void CalculateSpeakingTime()
+	{
+		speakingTime = Random.Range(minSpeakingTime, maxSpeakingTime);
+	}
+
+	void OnTriggerStay2D(Collider2D collider)
+	{
+		if(Input.GetKeyDown(KeyCode.LeftShift) && collider.name == "Boss" && isRinging)
+		{
+			CalculateSpeakingTime();
+
+			isRinging = false;
+			isBossSpeaking = true;
+
+			progressBar.GetComponent<StressProgressBar>().isVisible = true;
+		}
+		else if(Input.GetKeyUp(KeyCode.LeftShift) && collider.name == "Boss")
+		{
+			if(_totalTime == speakingTime)
+			{
+				_gameState.money += moneyPerSecond*speakingTime;
+				Debug.Log(moneyPerSecond* speakingTime);
+			}
+			isBossSpeaking = false;
+			_totalTime = 0;
+			progressBar.GetComponent<StressProgressBar>().isVisible = false;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D collider)
+	{
+		if(collider.name == "Boss")
+		{
+			isBossSpeaking = false;
+			_totalTime = 0;
+			progressBar.GetComponent<StressProgressBar>().isVisible = false;
+		}
+	}
+
+}
--- sgj/Assets/Scripts/StressProgressBar.cs
+++ sgj/Assets/Scripts/StressProgressBar.cs
@@ -7,6 +7,7 @@ public class StressProgressBar : MonoBehaviour {
     public float progress;
     public Vector2 size;
     public Vector2 offset;
+	public bool isVisible = true; 
 
 	// Use this for initialization
 	void Start () {
@@ -15,13 +16,16 @@ public class StressProgressBar : MonoBehaviour {
 	
 	// Update is called once per frame
 	void OnGUI () { 
-        var pos = Camera.main.WorldToScreenPoint(transform.position);
-        pos.y = Screen.height - (pos.y + offset.y);
-        pos.x = pos.x + offset.x;
-        GUI.DrawTexture(new Rect(pos.x, pos.y, size.x, size.y), progressBarEmpty);
-        GUI.BeginGroup(new Rect (pos.x, pos.y, size.x * Mathf.Clamp01(progress), size.y));
-        GUI.DrawTexture(new Rect(0, 0, size.x, size.y), progressBarFull);
-        GUI.EndGroup();
+		if(isVisible)
+		{
+	        var pos = Camera.main.WorldToScreenPoint(transform.position);
+	        pos.y = Screen.height - (pos.y + offset.y);
+	        pos.x = pos.x + offset.x;
+	        GUI.DrawTexture(new Rect(pos.x, pos.y, size.x, size.y), progressBarEmpty);
+	        GUI.BeginGroup(new Rect (pos.x, pos.y, size.x * Mathf.Clamp01(progress), size.y));
+	        GUI.DrawTexture(new Rect(0, 0, size.x, size.y), progressBarFull);
+	        GUI.EndGroup();
+		}
     }
 
     void ProgressUpdated(float val)
